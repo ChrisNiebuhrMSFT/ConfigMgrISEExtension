1c874563cfcfee2645290a8187acccf912800e01
Chris Niebuhr <Chris.Niebuhr@microsoft.com>
2018-07-22 18:05:18 +0000

    Consolidated Query SiteCode Eventhandler Using Styled in XAML for Datagrids

--- ConfigMgrExt/ConfigMgrMain_ContextMenuHandler.cs
+++ ConfigMgrExt/ConfigMgrMain_ContextMenuHandler.cs
@@ -10,10 +10,26 @@ namespace ConfigMgrExt
     /// </summary>
     public partial class ConfigMgrMain
     {
-        private void MenuSiteCode_WMIOnly(object sender, RoutedEventArgs e)
+        private void MenuQuery_SiteCode(object sender, RoutedEventArgs e)
         {
-            _logger.WriteLog("Query SiteCode WMI-Only Template was used");
-            ISEHelper.AddLine(HostObject, $"$siteCode = Get-WmiObject -Namespace root\\sms -Class SMS_ProviderLocation -ComputerName '{TxtSiteServer.Text}' -Filter \"ProviderForLocalSite=1\" |Select-Object -ExpandProperty SiteCode");
+            var tmpObject = sender as MenuItem;
+
+            switch (tmpObject.Name)
+            {
+                case "CtxQuerySiteCodeWMI":
+                    {
+                        _logger.WriteLog("Query SiteCode WMI-Only Template was used");
+                        ISEHelper.AddLine(HostObject, $"$siteCode = Get-WmiObject -Namespace root\\sms -Class SMS_ProviderLocation -ComputerName '{TxtSiteServer.Text}' -Filter \"ProviderForLocalSite=1\" |Select-Object -ExpandProperty SiteCode");
+                        break;
+                    }
+                case "CtxQuerySiteCodeCmdlet":
+                    {
+                        _logger.WriteLog("Query SiteCode with ConfigMgr Cmdlet Template was used");
+                        ISEHelper.AddLine(HostObject, "$siteCode = Get-CMSite | Select-Object -ExpandProperty SiteCode");
+                        break;
+                    }
+            }
+
         }
 
         private void MenuConfigMgr_CmdLet(object sender, RoutedEventArgs e)
@@ -22,29 +38,23 @@ namespace ConfigMgrExt
             ISEHelper.AddLine(HostObject, "If(-not(Get-Module ConfigurationManager))\n{\n\tImport-Module \"$($ENV:SMS_ADMIN_UI_PATH)\\..\\ConfigurationManager.psd1\"\n}\nSet-Location \"" + txtblcSiteCode.Text + ":\"");
         }
 
-        private void MenuSiteCode_CmdLet(object sender, RoutedEventArgs e)
-        {
-            _logger.WriteLog("Query SiteCode with ConfigMgr Cmdlet Template was used");
-            ISEHelper.AddLine(HostObject, "$siteCode = Get-CMSite | Select-Object -ExpandProperty SiteCode");
-        }
-
         private void Query_Application(object sender, RoutedEventArgs e)
         {
-            var tmpObject = (MenuItem)sender;
+            var tmpObject = sender as MenuItem;
             var application = (CMApplication)grdApplication.SelectedItem;
 
             switch (tmpObject.Name)
             {
-                case "CtxQueryApplicationWMI":
+                case "CtxQueryApplicationWMI_fast":
                     {
                         _logger.WriteLog("Query Application WMI-Only Template was used");
                         ISEHelper.AddLine(HostObject, $"$app = Get-WmiObject -Namespace root\\sms\\site_{txtblcSiteCode.Text} -Class SMS_ApplicationLatest -Filter \"LocalizedDisplayname='{application.LocalizedDisplayname}'\" -ComputerName {TxtSiteServer.Text}");
                         break;
                     }
-                case "CtxQueryApplicationCmdlet":
+                case "CtxQueryApplicationCmdlet_fast":
                     {
                         _logger.WriteLog("Query Application with ConfigMgr Cmdlet Template was used");
-                        ISEHelper.AddLine(HostObject, $"$app = Get-CMApplication -Name '{application.LocalizedDisplayname}'");
+                        ISEHelper.AddLine(HostObject, $"$app = Get-CMApplication -Name '{application.LocalizedDisplayname}' -Fast");
                         break;
                     }
             }
